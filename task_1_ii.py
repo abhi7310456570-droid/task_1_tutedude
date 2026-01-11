6ba5f58b73afed7b4c00ba28857e5bfdb7132d01
Abhishek Gupta <abhi7310456570@gmail.com>
2026-01-11 18:54:48 +0530

    Add files via upload

---
+++ task_1_ii.py
@@ -0,0 +1,4 @@
+first_name=str(input("enter the first name"))
+second_name=str(input("enter the second name"))
+full_name=first_name+" "+second_name
+print("hello every one my name is ",full_name,"and now i am learning python")
