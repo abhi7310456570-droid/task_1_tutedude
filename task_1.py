e4880ead054b9321b7158f989a3b5c71c0ae68b6
Abhishek Gupta <abhi7310456570@gmail.com>
2026-01-11 18:34:33 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,8 @@
+nums_1=int(input("enter the first numbers"))
+nums_2=int(input("enter the second numbers"))
+sum=nums_1+nums_2
+
+print("sum is ",sum)
+print("subtraction is ",nums_1-nums_2)
+print("multipuction is ",nums_1*nums_2)
+print("division is ",nums_1/nums_2)
