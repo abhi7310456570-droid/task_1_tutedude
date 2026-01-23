dc1fe0f57925d1fb72e923786dac9ebd77f42ee2
Abhishek Gupta <abhi7310456570@gmail.com>
2026-01-23 20:17:04 +0530

    Add files via upload

--- task_2_even_odd.py
+++ task_2_even_odd.py
@@ -3,3 +3,16 @@ if num%2==0:
     print("that's even number",num)
 else :
     print("that's odd number",num)
+
+
+#...................
+#......................
+#number from 1 to 50 by using for loop
+print("sum of intgers number from 1 to 50 by using for loop ")
+
+total = 0
+
+for i in range(1, 50):
+    total += i
+
+print("Sum from 1 to 50:", total)
